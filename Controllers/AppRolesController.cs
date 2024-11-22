858f960d421dce21097ce7e908902ada93144284
NikhilSaroop21 <128732882+NikhilSaroop21@users.noreply.github.com>
2024-11-22 10:41:24 +0200

    Update AppRolesController.cs

--- Controllers/AppRolesController.cs
+++ Controllers/AppRolesController.cs
@@ -48,9 +48,10 @@ namespace Nikhil_ST10040092_CLDV6212_Part3.Controllers
 // W3schools
 //https://www.w3schools.com/cs/index.php
 
-// code attribution
-// https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/start-mvc?view=aspnetcore-8.0&tabs=visual-studio
+// code attribution // W3schools // https://www.w3schools.com/cs/index.php
 
-// code attribution
-// https://learn.microsoft.com/en-us/aspnet/core/security/authentication/identity?view=aspnetcore-8.0&tabs=visual-studio
+// code attribution //Microsoft //https://learn.microsoft.com/en-us/aspnet/mvc/overview/getting-started/introduction/getting-started
 
+// code attribution //Microsoft //https://learn.microsoft.com/en-us/azure/storage/blobs/storage-blob-dotnet-get-started?tabs=azure-ad
+
+// code attribution //Bootswatch //https://bootswatch.com/
