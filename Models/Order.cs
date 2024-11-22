005d638fef530c10e0e33a70d5fa2abb41d1769c
NikhilSaroop21 <128732882+NikhilSaroop21@users.noreply.github.com>
2024-11-22 10:42:54 +0200

    Update Order.cs

--- Models/Order.cs
+++ Models/Order.cs
@@ -27,17 +27,10 @@ namespace Nikhil_ST10040092_CLDV6212_Part3.Models
         public string Order_Address { get; set; }
     }
 }
-//# Assistance provided by ChatGPT
-//# Code and support generated with the help of OpenAI's ChatGPT.
-// code attribution
-// W3schools
-//https://www.w3schools.com/cs/index.php
+// code attribution // W3schools // https://www.w3schools.com/cs/index.php
 
+// code attribution //Microsoft //https://learn.microsoft.com/en-us/aspnet/mvc/overview/getting-started/introduction/getting-started
 
+// code attribution //Microsoft //https://learn.microsoft.com/en-us/azure/storage/blobs/storage-blob-dotnet-get-started?tabs=azure-ad
 
-// code attribution
-// https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/start-mvc?view=aspnetcore-8.0&tabs=visual-studio
-
-// code attribution
-// https://learn.microsoft.com/en-us/aspnet/core/security/authentication/identity?view=aspnetcore-8.0&tabs=visual-studio
-
+// code attribution //Bootswatch //https://bootswatch.com/
